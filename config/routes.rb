47274753ba0b55b3fe5c8a8142cc94210579f7d3
Exequiel Rozas <exequiel98@gmail.com>
2024-10-07 02:52:37 -0400

    Change environment variable name from 'CLOUDFRONT_HOST0' to 'CDN_HOST'

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ Rails.application.routes.draw do
         :rails_service_blob_proxy,
         model.signed_id(expires_in: expires_in),
         model.filename,
-        options.merge(host: ENV['CLOUDFRONT_HOST'])
+        options.merge(host: ENV['CDN_HOST'])
       )
     else
       signed_blob_id = model.blob.signed_id(expires_in: expires_in)
@@ -50,7 +50,7 @@ Rails.application.routes.draw do
         signed_blob_id,
         variation_key,
         filename,
-        options.merge(host: ENV['CLOUDFRONT_HOST'])
+        options.merge(host: ENV['CDN_HOST'])
       )
     end
   end
