5043486bd4bda8bb7357e17f628f4593da4f00ec
Exequiel Rozas <exequiel98@gmail.com>
2024-10-07 03:09:44 -0400

    Fix issue with Cloudfront connection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,4 +103,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.hosts << ENV["PRODUCTION_HOST"]
+  config.hosts << ENV["CDN_HOST"]
 end
