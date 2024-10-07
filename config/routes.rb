567450be33b4452c3552f5f5d4b54c40649c8c02
Exequiel Rozas <exequiel98@gmail.com>
2024-10-06 23:07:44 -0400

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -13,25 +13,31 @@ Rails.application.routes.draw do
   
   direct :cdn_image do |model, options|
     expires_in = options.delete(:expires_in) { ActiveStorage.urls_expire_in }
-  
+
     if model.respond_to?(:signed_id)
-      route_for(
-        :rails_service_blob_proxy,
-        model.signed_id(expires_in: expires_in),
-        model.filename,
-        host: ENV["CLOUDFRONT_HOST"]
+      url_for(
+        Rails.application.routes.url_helpers.rails_service_blob_proxy_url(
+          model.signed_id(expires_in: expires_in),
+          model.filename,
+          host: ENV['CLOUDFRONT_HOST'],
+          protocol: 'https',
+          only_path: false
+        )
       )
     else
       signed_blob_id = model.blob.signed_id(expires_in: expires_in)
       variation_key  = model.variation.key
       filename       = model.blob.filename
-  
-      route_for(
-        :rails_blob_representation_proxy,
-        signed_blob_id,
-        variation_key,
-        filename,
-        host: ENV["CLOUDFRONT_HOST"]
+
+      url_for(
+        Rails.application.routes.url_helpers.rails_blob_representation_proxy_url(
+          signed_blob_id,
+          variation_key,
+          filename,
+          host: ENV['CLOUDFRONT_HOST'],
+          protocol: 'https',
+          only_path: false
+        )
       )
     end
   end
