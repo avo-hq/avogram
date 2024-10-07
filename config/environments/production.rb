0143b2fbd264b88471d8dd3555ccacc6b9e90321
Exequiel Rozas <exequiel98@gmail.com>
2024-10-06 21:13:00 -0400

    Remove master key requirement in order to obtain a working deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,10 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
+  config.action_controller.default_url_options = { host: "avogram.onrender.com" }
+  config.require_master_key = false
+
+
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
